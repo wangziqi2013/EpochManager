49212067c134f261eb506142dcd8af40ed5f61f4
= <675973885@qq.com>
2016-09-21 23:45:06 -0400

    Adding more framework

--- src/AtomicStack.h
+++ src/AtomicStack.h
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <cstdio>
+#include <atomic>
 
 namespace peloton {
 namespace index {
@@ -21,9 +22,39 @@ namespace index {
  */
 template <typename T>
 class AtomicStack {
+ private:
+  /*
+   * class Node - Stack node that forms a linked list
+   *
+   * Value will be copied constructed into this node when being pushed.
+   * Therefore type T must at least be copy-constructable
+   */
   class Node {
+   public:
+    T data;
     
+    // This does not have to be atomic since it only gets modified locally
+    // by the thread inserting into the stack
+    Node *next_p;
+    
+    /*
+     * Constructor - Initializes data member
+     */
+    Node(const T &p_data) :
+      data{p_data}
+    {}
   };
+  
+  // This will be modified using CAS both when inserting into and deleteing
+  // from the stack
+  std::atomic<Node *> head_p;
+  
+  /*
+   * Push() - Pushes a node into the stack
+   */
+  void Push(const T &data) {
+    Node node{data};
+  }
   
  public:
    
