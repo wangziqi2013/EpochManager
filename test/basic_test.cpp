6fc417ca5b44ef30bcea698d491bfb88bd772a84
Ziqi Wang <675973885@qq.com>
2016-09-22 03:00:52 -0400

    Adding ThreadTest

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,5 +1,6 @@
 
 #include "../src/AtomicStack.h"
+#include <thread>
 
 using namespace peloton;
 using namespace index;
@@ -23,9 +24,43 @@ void BasicTest() {
   return;
 }
 
+void ThreadTest(int thread_num, int op_num) {
+  dbg_printf("========== Thread Test ==========\n");
+  
+  AtomicStack<int> as{};
+  
+  auto push_func = [&as, op_num](uint64_t id) {
+                     for(int i = static_cast<int>(id);i < thread_num * op_num;i += thread_num) {
+                       as.Push(i);
+                     }
+                   }
+
+  // We use this to count what we have fetched from the stack
+  std::atomic<int> sum;
+  sum.store(0);
+
+  auto pop_func = [&as, &sum, op_num](uint64_t id) {
+                    for(int i = 0;i < op_num;i++) {
+                      int data;
+                      as.Pop(data));
+                      
+                      // Atomically adding the poped value onto the atomic
+                      sum.fetch_add(data);
+                    }
+                  }
+                  
+  StartThreads(thread_num, push_func);
+  StartThreads(thread_num, pop_func);
+  
+  dbg_printf("Sum = %d\n", sum.load());
+  assert(sum.load() == (op_num * thread_num));
+  
+  return;
+}
 
 int main() {
   BasicTest();
+  ThreadTest();
   
   return 0;
 }
