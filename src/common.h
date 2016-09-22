625ee87a60fa90508984cd0dae009e10d6beb51b
Ziqi Wang <675973885@qq.com>
2016-09-22 02:53:28 -0400

    Adding StartThreads() template to the header

--- src/common.h
+++ src/common.h
@@ -29,3 +29,22 @@ static void dummy(const char*, ...) {}
 #define likely(x)   __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
+// Template function to launch threads
+// This does not define anything but to specify a template so it is
+// OK to put it here
+template <typename Fn, typename... Args>
+void StartThreads(uint64_t num_threads, Fn&& fn, Args &&... args) {
+  std::vector<std::thread> thread_group;
+
+  // Launch a group of threads
+  for (uint64_t thread_itr = 0; thread_itr < num_threads; ++thread_itr) {
+    thread_group.push_back(std::thread(fn, thread_itr, args...));
+  }
+
+  // Join the threads with the main thread
+  for (uint64_t thread_itr = 0; thread_itr < num_threads; ++thread_itr) {
+    thread_group[thread_itr].join();
+  }
+  
+  return;
+}
