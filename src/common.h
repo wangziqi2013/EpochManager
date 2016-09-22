9ac78a0ca6775a9ba2c18385e7a023449e81a674
Ziqi Wang <675973885@qq.com>
2016-09-22 03:01:10 -0400

    Change a little bit of StartThreads

--- src/common.h
+++ src/common.h
@@ -35,17 +35,17 @@ static void dummy(const char*, ...) {}
 // This does not define anything but to specify a template so it is
 // OK to put it here
 template <typename Fn, typename... Args>
-void StartThreads(uint64_t num_threads, Fn&& fn, Args &&... args) {
-  std::vector<std::thread> thread_group;
+void StartThreads(uint64_t num_threads, Fn &&fn, Args &&... args) {
+  std::vector<std::thread> thread_list;
 
   // Launch a group of threads
-  for (uint64_t thread_itr = 0; thread_itr < num_threads; ++thread_itr) {
-    thread_group.push_back(std::thread(fn, thread_itr, args...));
+  for(uint64_t thread_id = 0;thread_id < num_threads;thread_id++) {
+    thread_list.push_back(std::thread(fn, thread_id, args...));
   }
 
   // Join the threads with the main thread
-  for (uint64_t thread_itr = 0; thread_itr < num_threads; ++thread_itr) {
-    thread_group[thread_itr].join();
+  for(uint64_t thread_id = 0;thread_id < num_threads;thread_id++) {
+    thread_list[thread_id].join();
   }
   
   return;
