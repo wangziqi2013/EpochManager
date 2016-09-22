3f656e935e445ffd68aa97b2164966ea27031efe
= <675973885@qq.com>
2016-09-21 23:29:50 -0400

    Adding AtomicStack framework

---
+++ src/AtomicStack.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "common.h"
+
+#include <vector>
+#include <cstdio>
+
+/*
+ * AtomicStack - A lock-free stack that supports concurrent access of multiple
+ *               threads
+ *
+ * This class is implemented to assist in designing of an efficient and
+ * scalable epoch based garbage collector. It could also be used as a general
+ * purpose lock free stack data structure, disregarding the bottleneck brought
+ * by malloc() and pointer chasing (so it might not worth the effort of making
+ * it lock-free)
+ */
+template <typename T>
+class AtomicStack {
+  class Node {
+    
+  };
+  
+ public:
+   
+};
