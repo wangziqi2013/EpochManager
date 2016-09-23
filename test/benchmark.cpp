4fd4b858c39228649eb818fb8f1c461301622edc
= <675973885@qq.com>
2016-09-22 21:32:08 -0400

    Adding benchmark test case for EM factory

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -1,6 +1,18 @@
 
 #include "../src/AtomicStack.h"
+#include "../src/LocalWriteEM.h"
+
+template <uint64_t core_num>
+std::unordered_map<void *, void *>
+LocalWriteEMFactory<core_num>::instance_map{};
 
 int main() {
+  LocalWriteEM<4> *p = LocalWriteEMFactory<4>::GetInstance();
+  
+  dbg_printf("pointer = %p\n", p);
+  dbg_printf("Multiple of 64? %d\n", (int)!((uint64_t)p % 64));
+  
+  LocalWriteEMFactory<4>::FreeInstance(p);
+  
   return 0;
 }
