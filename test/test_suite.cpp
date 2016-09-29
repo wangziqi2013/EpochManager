9a6f22a4bb664d3c91b07a3f484bc485a112d6f2
= <675973885@qq.com>
2016-09-28 23:31:48 -0400

    Adding SleepFor()

--- test/test_suite.cpp
+++ test/test_suite.cpp
@@ -9,3 +9,13 @@ void PrintTestName(const char *name) {
   
   return;
 }
+
+/*
+ * SleepFor() - Sleeps in the main thread
+ */
+void SleepFor(uint64_t sleep_ms) {
+  std::chrono::milliseconds duration{sleep_ms};
+  std::this_thread::sleep_for(duration);
+  
+  return;
+}
--- test/test_suite.h
+++ test/test_suite.h
@@ -4,3 +4,4 @@
 #include "../src/common.h"
 
 void PrintTestName(const char *name);
+void SleepFor(uint64_t sleep_ms); 
