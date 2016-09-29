94e15f029758653da75e546427577c0b96389280
Ziqi Wang <675973885@qq.com>
2016-09-29 16:42:37 -0400

    Moving StartThreads() to test suite

--- src/common.h
+++ src/common.h
@@ -35,22 +35,3 @@ static void dummy(const char*, ...) {}
 #define likely(x)   __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
-// Template function to launch threads
-// This does not define anything but to specify a template so it is
-// OK to put it here
-template <typename Fn, typename... Args>
-void StartThreads(uint64_t num_threads, Fn &&fn, Args &&... args) {
-  std::vector<std::thread> thread_list;
-
-  // Launch a group of threads
-  for(uint64_t thread_id = 0;thread_id < num_threads;thread_id++) {
-    thread_list.push_back(std::thread(fn, thread_id, args...));
-  }
-
-  // Join the threads with the main thread
-  for(uint64_t thread_id = 0;thread_id < num_threads;thread_id++) {
-    thread_list[thread_id].join();
-  }
-  
-  return;
-}
--- test/test_suite.h
+++ test/test_suite.h
@@ -5,3 +5,23 @@
 
 void PrintTestName(const char *name);
 void SleepFor(uint64_t sleep_ms); 
+
+// Template function to launch threads
+// This does not define anything but to specify a template so it is
+// OK to put it here
+template <typename Fn, typename... Args>
+void StartThreads(uint64_t num_threads, Fn &&fn, Args &&... args) {
+  std::vector<std::thread> thread_list;
+
+  // Launch a group of threads
+  for(uint64_t thread_id = 0;thread_id < num_threads;thread_id++) {
+    thread_list.push_back(std::thread(fn, thread_id, args...));
+  }
+
+  // Join the threads with the main thread
+  for(uint64_t thread_id = 0;thread_id < num_threads;thread_id++) {
+    thread_list[thread_id].join();
+  }
+  
+  return;
+}
