fa386a3de40c0b638b769f78bbdf979170d1ce44
Ziqi Wang <675973885@qq.com>
2016-09-30 15:54:32 -0400

    Adding SimpleBenchmark

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -27,8 +27,42 @@ void GetThreadAffinityBenchmark() {
   
   dbg_printf("Time usage (iter %d) = %f\n", iter, duration);
   dbg_printf("    Throughput = %f op/second\n", 
-             static_cast<double>(iter) / duration);
+             static_cast<double>(iter) / duration); 
              
+  return;
+}
+
+/*
+ * SimpleBenchmark() - Benchmark how EM works without workload - just announce
+ *                     entry & exit and it's all       
+ */
+void SimpleBenchmark() {
+  PrintTestName("SimpleBenchmark");
+
+  // Make sure thread number is an even number otherwise exit
+  if((thread_num % 2) != 0) {
+    dbg_printf("SimpleBenchmark requires thread_num being an even number!\n");
+    
+    return;
+  }
+  
+  // This instance must be created by the factory
+  EM *em = EMFactory::GetInstance();
+
+  auto func = [em, thread_num, op_num](uint64_t id) {
+                // This is the core ID this thread is logically pinned to
+                // physically we do not make any constraint here
+                uint64_t core_id = id % CoreNum;
+                
+                em->AnnounceEnter(core_id);
+              };
+
+  // Let threads start
+  StartThreads(thread_num, func);
+
+
+  EMFactory::FreeInstance(em);
+
   return;
 }
 
