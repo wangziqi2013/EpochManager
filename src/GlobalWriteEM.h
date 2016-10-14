5e8ac91e295e3fb6c3a2764e69904c91836aa79a
= <675973885@qq.com>
2016-10-13 23:11:08 -0400

    Adding include

--- src/GlobalWriteEM.h
+++ src/GlobalWriteEM.h
@@ -1,10 +1,10 @@
 
 #pragma once
 
-#include "common.h"
-
 #ifndef _GLOBAL_WRITE_EM_H
 #define _GLOBAL_WRITE_EM_H
+
+#include "common.h"
 
 /*
  * class GlobalWriteEM - An implementation of epoch-based safe memory 
