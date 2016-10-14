fbac843d82d94f6fc7adc9e34ae00a632c34dc9e
= <675973885@qq.com>
2016-10-14 00:52:02 -0400

    Adding basic framework into global_em_test.cpp

--- test/global_em_test.cpp
+++ test/global_em_test.cpp
@@ -3,9 +3,12 @@
  * global_em_test.cpp - Correctness test for global counter EM 
  */
 
+#include "../src/AtomicStack.h"
 #include "../src/GlobalWriteEM.h"
-#include "../src/LocalWriteEM.h"
 #include "test_suite.h"
+
+using namespace peloton;
+using namespace index;
 
 // Declear stack and its node type here
 using StackType = AtomicStack<uint64_t>;
