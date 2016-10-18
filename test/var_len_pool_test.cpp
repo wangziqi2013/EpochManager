3b35a32900d816c69d337e0ec51da8a60f165b6a
Ziqi Wang <675973885@qq.com>
2016-10-18 12:01:34 -0400

    Adding VarLenPoolBasicTest

--- test/var_len_pool_test.cpp
+++ test/var_len_pool_test.cpp
@@ -5,17 +5,39 @@
 
 #include "test_suite.h"
 
+/*
+ * VarLenPoolBasicTest() - This function allocates a series of memory
+ *                         using an increasing sequence and then check
+ *                         for correctness
+ */
 void VarLenPoolBasicTest() {
   PrintTestName("VarLenPoolBasicTest");
   
+  static const iter = 10;
+  
+  void *p_list[iter];
+  
   // Chunk size = 64
   VarLenPool vlp{64};
+  for(int i = 1;i <= 10;i++) {
+    p_list[i - 1] = vlp.Allocate(i);
+    memset(p_list[i - 1], i, i);
+  }
   
+  // Verify for correctness
+  for(int i = 1;i <= 10;i++) {
+    char *p = reinterpret_cast<char *>(p_list[i - 1]);
+    for(int j = 0;j < i;j++) {
+      assert(p[j] == static_cast<char>(i));
+    }
+  }
   
   return; 
 }
 
 
 int main() {
+  VarLenPoolBasicTest();
   
+  return;
 }
