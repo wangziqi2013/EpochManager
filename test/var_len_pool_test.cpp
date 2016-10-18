75833cc4b2fd27cd216b68dd974eb86c9c940b2c
Ziqi Wang <675973885@qq.com>
2016-10-18 11:56:59 -0400

    Adding test for class VarLenPool

---
+++ test/var_len_pool_test.cpp
@@ -0,0 +1,21 @@
+
+/*
+ * var_len_pool_test.cpp - Test suite for varrable lengthed allocation pool
+ */
+
+#include "test_suite.h"
+
+void VarLenPoolBasicTest() {
+  PrintTestName("VarLenPoolBasicTest");
+  
+  // Chunk size = 64
+  VarLenPool vlp{64};
+  
+  
+  return; 
+}
+
+
+int main() {
+  
+}
